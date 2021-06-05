9bee20727028dbefd690ca2fe74e40deb9091ef7
Dusan Ilic <idtronicsrs@gmail.com>
2021-06-05 23:41:27 +0200

    Added dunstrc, plus minor changes

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -5,8 +5,9 @@ sxhkd -m 1 &
 # Monitor settings
 xrandr --output HDMI1 --primary --left-of eDP1
 # Start the notification-daemon
-/usr/lib/notification-daemon-1.0/notification-daemon &
+# /usr/lib/notification-daemon-1.0/notification-daemon &
 # /usr/lib/xfce4/notifyd/xfce4-notifyd &
+# dunst &
 # Allow transparent windows
 xcompmgr &
 # Initialize keyboard layout
